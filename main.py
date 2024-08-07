379bbf6fda6cc22da801003543a56705670fcf96
Muhammad Talha <[200~muhammadtalhayoung1@gmail.com>
2024-08-07 22:59:28 +0500

    Tried to convert code into OOP

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from pygame.locals import *
 
+class snake:
+    pass
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        surface = pygame.display.set_mode((1000, 500))
+        surface.fill((110, 110, 5))
+
 def draw_block():
     surface.fill((110, 110, 5))
     surface.blit(block, (block_x, block_y))
     pygame.display.flip()
 
 if __name__ == '__main__':
-    pygame.init()
     
-    surface = pygame.display.set_mode((1000, 500))
-    surface.fill((110, 110, 5))
 
     block  = pygame.image.load("resources/block.jpg").convert()
     block_x = 100
